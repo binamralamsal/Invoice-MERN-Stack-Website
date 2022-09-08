9d6a2c26fd16cbf4b94248044c3ba734bdfd5532
binamralamsal <binamralamsal@gmail.com>
2022-09-08 22:44:44 +0545

    Swagger testing

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import express, { Application } from "express";
 import config from "./config";
 import Routes from "./interfaces/routes.interface";
 import { error } from "./middlewares";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 class App {
   public app: Application;
@@ -13,6 +15,7 @@ class App {
 
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
+    this.initializeSwagger();
     this.initializeErrorHandling();
   }
 
@@ -49,6 +52,22 @@ class App {
     routes.forEach((route) => {
       this.app.use(route.path, route.router);
     });
+  }
+
+  private initializeSwagger() {
+    const options = {
+      swaggerDefinition: {
+        info: {
+          title: "REST API",
+          version: "1.0.0",
+          description: "Example docs",
+        },
+      },
+      apis: ["swagger.yaml"],
+    };
+
+    const specs = swaggerJSDoc(options);
+    this.app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(specs));
   }
 
   private initializeErrorHandling() {
